747747399c41f6808582410dec35b94b5084cad7
TEST <TEST@DESKTOP-2SP86VM>
2023-12-02 11:08:18 -0500

    Prueba Tecnica Global Bank

---
+++ TEST/src/Palindromo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Palindromo {
+
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Bienvenido, Introduce una cadena de texto: ");
+	        String texto = sc.nextLine();
+
+	        boolean Validacion = true;
+	        for (int i = 0; i < texto.length() / 2; i++) {
+	            if (texto.charAt(i) != texto.charAt(texto.length() - i - 1)) {
+	                Validacion = false;
+	                break;
+	            }
+	        }
+
+	        if (Validacion) {
+	            System.out.println("La cadena es un palíndromo");
+	        } else {
+	            System.out.println("La cadena no es un palíndromo");
+	        }
+	    }
+	}
